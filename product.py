509cb979585b93d218be9a48bf73c1b677471b53
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2022-07-18 10:58:45 +0200

    FIX search and if.

--- product.py
+++ product.py
@@ -70,11 +70,8 @@ class Template(metaclass=PoolMeta):
                     # Check if we have more than one child category for each
                     # unique category
                     childs = Category.search([
-                        ('parent', 'child_of', unique_category_id),
-                        ('id', '!=', unique_category_id)])
-                    childs_in_template = [u.id for u in filter(
-                        lambda a: a in childs, template.categories)]
-                    if len(childs_in_template) > 1:
+                        ('parent', 'child_of', unique_category_id)])
+                    if len(set(childs) & set(template.categories)) > 1:
                         raise UserError(gettext(
                             'product_categories.repeated_unique',
                             template=template.rec_name))
